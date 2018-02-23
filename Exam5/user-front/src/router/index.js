581fdd55c7fb71b1273dae09db4e9d0b9a481cae
huangguo <huangguo@xiaomi.com>
2018-02-23 17:28:48 +0800

    wawa

--- Exam5/user-front/src/router/index.js
+++ Exam5/user-front/src/router/index.js
@@ -1,75 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import NProgress from 'nprogress'
-
-const Home = resolve => require(['@/views/Home'], resolve)
-const About = resolve => require(['@/views/About'], resolve)
-const Html = resolve => require(['@/views/Html'], resolve)
-const Exam = resolve => require(['@/views/Exam'], resolve)
-const ExamDetail = resolve => require(['@/views/ExamDetail'], resolve)
-const ExamEdit = resolve => require(['@/views/ExamAdd'], resolve)
-const ExamAdd = resolve => require(['@/views/ExamAdd'], resolve)
-const Question = resolve => require(['@/views/Question'], resolve)
-const Error404 = resolve => require(['@/views/error/Error404'], resolve)
+import qsList from '@/components/QS-list'
+import qsData from '@/components/QS-data'
+import qsFill from '@/components/QS-fill'
+import qsEdit from '@/components/QS-edit'
 
 Vue.use(Router)
 
-let routes = [
+export default new Router({
+  routes: [
     {
-        path: '/',
-        component: Home
+      path: '/',
+      name: 'qsList',
+      component: qsList
     },
     {
-        path: '/about',
-        component: About
+      path: '/data/:num',
+      name: 'qsData',
+      component: qsData
     },
     {
-        path: '/cat/html',
-        component: Html
+      path: '/fill/:num',
+      name: 'qsFill',
+      component: qsFill
     },
     {
-        path: '/question',
-        component: Question
-    },
-    {
-        path: '/exam/add',
-        component: ExamAdd
-    },
-    {
-        path: '/exams/:id',
-        component: ExamDetail
-    },
-    {
-        path: '/exams/:id/edit',
-        component: ExamEdit
-    },
-    {
-        path: '/exams',
-        component: Exam
-    },
-    {
-        path: '*',
-        component: Error404
+      path: '/edit/:num',
+      name: 'qsEdit',
+      component: qsEdit
     }
-]
-
-let router = new Router({
-    mode: 'history',
-    routes: routes,
-    scrollBehavior (to, from, savedPosition) {
-        return {
-            x: 0,
-            y: 0
-        }
-    }
+  ]
 })
-
-router.beforeEach((to, from, next) => {
-    NProgress.start()
-    next()
-})
-router.afterEach(transition => {
-    NProgress.done()
-})
-
-export default router
